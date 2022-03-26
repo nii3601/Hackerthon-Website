582dc55ac8b2ab8a9a61f77dfe9f1772007169a8
Jonathan Lamptey <jlamptey@nd.edu>
2022-03-25 22:15:26 -0400

    Finished Navbar

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,15 +1,24 @@
-import { Navbar,Nav } from 'react-bootstrap-navbar';
-import { Container } from 'react-bootstrap';
-import './Navigator.css';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import './Navigation.css';
 
-function Navigator(){
-    return(
-        <div className='Navigator'>
-            <div>
-                <h1>Text</h1>
-            </div>
+function Navigation() {
+    return (
+        <div className='Navigation'>
+            <Navbar collapseOnSelect id='Nav-Back' expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="/">Hackathon Project</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link href="/rules" id="items">Rules</Nav.Link>
+                            <Nav.Link href="/leaderboard" id="items">Leaderboard</Nav.Link>
+                            <Nav.Link href="/register" id="items">Register</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     );
 }
 
-export default Navigator;
+export default Navigation;
---
+++ src/Pages/LeaderBoard/LeaderBoard.js
@@ -0,0 +1,12 @@
+import "./LeaderBoard.css";
+
+function Leaderboard({hasDorm}){
+    console.log(hasDorm)
+    return(
+        <div className="LeaderBoard">
+            <h3>LeaderBoard Page</h3>
+        </div>
+    );
+}
+
+export default Leaderboard;
