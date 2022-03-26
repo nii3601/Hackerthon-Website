fb5c090f32118f40aa132a420679ddcfbda96eb8
Jonathan Lamptey <jlamptey@nd.edu>
2022-03-25 21:04:58 -0400

    added naviagtion

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './Pages/Home/Home';
+import {Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,8 +19,11 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-      <Home/>
+      </header> */}
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+      </Routes>
+      
     </div>
   );
 }
