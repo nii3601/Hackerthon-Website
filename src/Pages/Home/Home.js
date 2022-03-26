e27d9ca11721c37f533d994cb0aaf9663a05850d
dadams2001 <dadams22@nd.edu>
2022-03-26 18:32:04 -0400

    Added Home.js and Home.css

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,86 @@
-import {Container,Row} from 'react-bootstrap';
+import {Container,Row,Col} from 'react-bootstrap';
+import LeaderBoard from '../LeaderBoard/LeaderBoard'
+import './Home.css'
 
 function Home(){
     return(
         <div className="Home">
             <Container fluid>
-                <Row>
-                    <h1>This is home page</h1>
-                </Row>
+                <Col>
+                    <div id="not_leaderboard">
+                        <Row>
+                            <Row>
+                                <Col>
+                                    <h1 
+                                        id = "h1_home"
+                                        style={{
+                                            "textAlign":"left"
+                                        }}
+                                    >
+                                        Average Campus Water Use Per Day:
+                                    </h1>
+                                </Col>
+                                <Col>
+                                    <h1 
+                                        id = "h1_home"
+                                        style = {{
+                                            "textAlign":"left"
+                                        }}
+                                    >
+                                        50
+                                    </h1>
+                                    <h2
+                                        id = "h2_home"
+                                    >
+                                        gal/day
+                                    </h2>
+                                </Col>
+                            </Row>
+                            <div id="row2">
+                                <Row>
+                                    <Col>
+                                        <h2
+                                            id = "h2_home"
+                                            style={{
+                                                "textAlign":"left"
+                                            }}
+                                        >
+                                            Total Campus Water Use This Year:
+                                        </h2>
+                                    </Col>
+                                    <Col>
+                                        <h2
+                                            id = "h2_home"
+                                        >
+                                            50
+                                        </h2>
+                                        <h2
+                                            id = "h2_home"
+                                        >
+                                            gal
+                                        </h2>
+                                    </Col>
+                                </Row>
+                            </div>
+                        </Row>
+                        <br/>
+                        <br/>
+                        <Row>
+                            <Col>
+                                <h1 id = "h1_home">Type the name of a dorm here:</h1>
+                                <input 
+                                    type="text" 
+                                    placeholder="Search..."
+                                />
+                            </Col>
+                        </Row>
+                    </div>
+                    <br/>
+                    <br/>
+                    <Row>
+                        <LeaderBoard hasDorm={false}/>
+                    </Row>
+                </Col>
             </Container>
         </div>
     );
