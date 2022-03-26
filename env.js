aec582d82a6eb020bfdc699f6e5bd67ef78413c3
Jonathan Lamptey <jlamptey@nd.edu>
2022-03-25 20:53:27 -0400

    Team work setup

---
+++ env.js
@@ -0,0 +1,8 @@
+module.exports = {
+    APPLICATION_ID: "YOUR_APP_ID",
+    JAVASCRIPT_KEY: "YOUR_JS_KEY",
+    SERVER_URL: "https://parseapi.back4app.com/",
+    BACKGROUND: "#000000",
+    FOREGROUND: "#FFFFFF",
+    FONT: "Press Start 2P"
+}
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,11 @@
+
+
+function Home(){
+    return(
+        <div className="Home">
+            <h1>This is home page</h1>
+        </div>
+    );
+}
+
+export default Home;
