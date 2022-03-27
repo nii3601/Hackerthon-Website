37c5317d98f44ec244f64da4e3e22ecd2f6de348
Jonathan Lamptey <jlamptey@nd.edu>
2022-03-27 10:03:10 -0400

    get dorm names

--- src/Services/getDormNames.js
+++ src/Services/getDormNames.js
@@ -1,6 +1,10 @@
 import Parse from "parse";
 
 
-export const getDormNames = (dormID) => {
-    
+export const getDormNames = () => {
+    const Dorm = Parse.Object.extend("dorms");
+    const query = new Parse.Query(Dorm);
+    return query.find().then((results)=>{
+        return results;
+    })
 }
